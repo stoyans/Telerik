ad1943834ff35650c8482c20295fc8d49c498b84
stoyans <fi_tanis@abv.bg>
2014-02-11 02:01:59 +0200

    oop

---
+++ OOP/ExtensionMethods_Lambda_LINQ/03.LastNameBeforeFirstName/Students.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.LastNameBeforeFirstName
+{
+    class Student
+    {
+        private string firstName;
+        private string secondName;
+        private int age;
+
+        public Student(string fname, string sname, int age)
+        {
+            this.FirstName = fname;
+            this.SecondName = sname;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string SecondName
+        {
+            get
+            {
+                return this.secondName;
+            }
+            set
+            {
+                this.secondName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+    }
+}
--- OOP/ExtensionMethods_Lambda_LINQ/03.LastNameBeforeFirstName/TestProgram.cs
+++ OOP/ExtensionMethods_Lambda_LINQ/03.LastNameBeforeFirstName/TestProgram.cs
@@ -10,11 +10,49 @@ namespace _03.LastNameBeforeFirstName
     {
         static void Main()
         {
-            List<string> students = new List<string>();
-            students.Add("Petar Savov");
-            students.Add("Sava Petrov");
-            students.Add("Mark Kind");
-            students.Add("Green Forest");
+            Student student1 = new Student("Mark", "Twen", 25);
+            Student student2 = new Student("Mark", "Knight", 24);
+            Student student3 = new Student("Green", "Forest", 22);
+            Student student4 = new Student("High", "Cloud", 21);
+
+            List<Student> listOfStundets = new List<Student>();
+            listOfStundets.Add(student1);
+            listOfStundets.Add(student2);
+            listOfStundets.Add(student3);
+            listOfStundets.Add(student4);
+
+
+            var sorted =
+            from st in listOfStundets
+            where st.FirstName.CompareTo(st.SecondName) == -1
+            select (st.FirstName + " " + st.SecondName);
+
+            var alphabeticallyStudents = listOfStundets.Where(st => st.FirstName.CompareTo(st.SecondName) < 0);
+
+            var sortedAge =
+            from st in listOfStundets
+            where st.Age.CompareTo(18) >= 0 && st.Age.CompareTo(24) <= 0
+            select (st.FirstName + " " + st.SecondName);
+
+            var sortedAgeLINQ = listOfStundets.Where(st => st.Age.CompareTo(18) >= 0 && st.Age.CompareTo(24) <= 0);
+
+
+            foreach (var item in alphabeticallyStudents)
+            {
+                Console.WriteLine("{0} {1}", item.FirstName, item.SecondName);
+            }
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in sortedAgeLINQ)
+            {
+                Console.WriteLine("{0} {1}", item.FirstName, item.SecondName);
+            }
+
+
         }
     }
 }
