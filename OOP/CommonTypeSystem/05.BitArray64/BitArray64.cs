ecb06be5327d36bf3485d2d30319f5e481e9c11e
stoyans <fi_tanis@abv.bg>
2014-02-22 23:47:11 +0200

    oop task

---
+++ OOP/CommonTypeSystem/05.BitArray64/BitArray64.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _05.BitArray64
+{
+    class BitArray64 : IEnumerable<int>
+    {
+
+        private int[] bitArray = new int[64];
+        private ulong number;
+
+        public ulong Number
+        {
+            get { return this.number; }
+            private set { this.number = value; }
+        }
+
+        public BitArray64(ulong number)
+        {
+            this.Number = number;
+
+            for (int i = 0; i < 64; i++)
+            {
+                bitArray[i] = (int)(number % 2);
+                number /= 2;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int[] bits = this.bitArray;
+            for (int i = 63; i >= 0; i--)
+            {
+                yield return bits[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is BitArray64))
+            {
+                return false;
+            }
+
+            BitArray64 other = obj as BitArray64;
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return this.Number == other.Number;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > 63)
+                {
+                    throw new System.IndexOutOfRangeException();
+                }
+
+                int[] bits = this.bitArray;
+                return bits[index];
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(BitArray64 firstNum, BitArray64 secondNum)
+        {
+            return firstNum.Equals(secondNum);
+        }
+
+        public static bool operator !=(BitArray64 firstNum, BitArray64 secondNum)
+        {
+            return !(firstNum == secondNum);
+        }
+    }
+}
--- OOP/CommonTypeSystem/05.BitArray64/Program.cs
+++ OOP/CommonTypeSystem/05.BitArray64/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05.BitArray64
 {
@@ -10,7 +7,21 @@ namespace _05.BitArray64
     {
         static void Main()
         {
+            BitArray64 number = new BitArray64(20);
+            BitArray64 number2 = new BitArray64(7);
 
+            foreach (var bit in number)
+            {
+                Console.Write(bit);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Bit at position 2 is {0}", number[2]);
+
+            Console.WriteLine("Hash code of {0} is {1}", number.Number, number.GetHashCode());
+
+            Console.WriteLine("number == number2 -> {0}", number == number2);
+            Console.WriteLine("number != number2 -> {0}", number != number2);
+            Console.WriteLine("number equals number2 -> {0}", number.Equals(number2));
         }
     }
 }
