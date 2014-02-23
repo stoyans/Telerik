9c7dd2656dbe28df06815b0a7542dd713be412a3
stoyans <fi_tanis@abv.bg>
2014-02-23 14:21:35 +0200

    edit

--- Programming/CSharpPart1/Intro_Programming/Task_2_Hello/Task_2_Hello.cs
+++ Programming/CSharpPart1/Intro_Programming/Task_2_Hello/Task_2_Hello.cs
@@ -7,8 +7,6 @@ namespace Task_2_Hello
         static void Main()
         {
             Console.WriteLine("Hello C#");
-            string text = "Всички задачи са добре форматирани и четими. Задачи 2, 3, 4 - структуриран код Добро решение на задача 9 и 7 Задача 8 би могла да се реализира и с метода Math.Pow()Успех и занапред!";
-            Console.WriteLine(text.Length);
         }
     }
 }
