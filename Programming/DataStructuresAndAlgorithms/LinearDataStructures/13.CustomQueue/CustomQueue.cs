35a36bf9a756fc2fe3cc794b12dfacda7b1f54cf
stoyans <fi_tanis@abv.bg>
2014-04-06 16:22:29 +0300

    DSA update

---
+++ Programming/DataStructuresAndAlgorithms/LinearDataStructures/13.CustomQueue/CustomQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.CustomQueue
+{
+    public class CustomQueue<T>
+    {
+        private LinkedList<T> queue;
+
+        public CustomQueue()
+        {
+            queue = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            this.queue.AddLast(item);
+        }
+
+        public T Peek()
+        {
+            return this.queue.First.Value;
+        }
+
+        public T Dequeue()
+        {
+            Peek();
+            T lastItem = this.queue.First.Value;
+            this.queue.RemoveFirst();
+            return lastItem;
+        }
+
+        public int Count
+        {
+            get { return this.queue.Count; }
+        }
+    }
+}
---
+++ Programming/DataStructuresAndAlgorithms/LinearDataStructures/13.CustomQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.CustomQueue
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<int> abv = new Queue<int>();
+            //abv.Pe
+            CustomQueue<int> customList = new CustomQueue<int>();
+            customList.Enqueue(3);
+            customList.Enqueue(5);
+            customList.Enqueue(1);
+            Console.WriteLine(customList.Dequeue());
+            Console.WriteLine(customList.Dequeue());
+            customList.Enqueue(12);
+            Console.WriteLine(customList.Dequeue());
+            Console.WriteLine(customList.Count);
+        }
+    }
+}
