b8666b05162f4336a7a147da805f0cf39c0c20cb
Stoyan Stoyanov <fi_tanis@abv.bg>
2014-06-02 16:32:01 +0300

    update js

--- WebDesign/JavaScript UI and DOM/02.HTML5Canvas/01.DrawCanvasGraphics/draw-With-Canvas.js
+++ WebDesign/JavaScript UI and DOM/02.HTML5Canvas/01.DrawCanvasGraphics/draw-With-Canvas.js
@@ -2,6 +2,7 @@
     var canvas = document.getElementById('canvas-context');
     var canvasContext = canvas.getContext('2d');
 
+    //DRAW HOUSE
     //draw body house
     canvasContext.beginPath();
     canvasContext.rect(600, 300, 380, 300);
@@ -64,6 +65,8 @@
     canvasContext.lineTo(870, 150);
     canvasContext.quadraticCurveTo(890, 140, 910, 150);
     canvasContext.lineTo(910, 250);
+    canvasContext.moveTo(910, 150);
+    canvasContext.quadraticCurveTo(890, 158, 870, 150);
     canvasContext.fillStyle = '#FF96B9';
     canvasContext.fill();
     canvasContext.stroke();
@@ -89,6 +92,8 @@
     canvasContext.stroke();
 
     canvasContext.beginPath();
+    canvasContext.lineCap = 'round';
+    canvasContext.lineJoin = 'round';
     canvasContext.moveTo(760, 600);
     canvasContext.lineTo(760, 490);
     canvasContext.moveTo(760, 490);
@@ -96,5 +101,96 @@
     canvasContext.moveTo(640, 490);
     canvasContext.lineTo(640, 600);
     canvasContext.stroke();
+    canvasContext.closePath();
+
+    //DRAW HUMAN
+    //draw head
+    canvasContext.beginPath();
+    canvasContext.arc(200, 300, 60, 0, 2 * Math.PI);
+    canvasContext.strokeStyle = '#37B6FA';
+    canvasContext.fillStyle = '#9DECF5';
+    canvasContext.fill();
+    canvasContext.stroke();
+    canvasContext.closePath();
+    //draw eyes
+    canvasContext.beginPath();
+    canvasContext.save();
+    canvasContext.scale(1, 0.7);
+    canvasContext.arc(170, 400, 10, 0, 2 * Math.PI, false);
+    canvasContext.stroke();
+    canvasContext.closePath();
+    canvasContext.beginPath();
+    canvasContext.arc(220, 400, 10, 0, 2 * Math.PI, false);
+    canvasContext.stroke();
+    canvasContext.closePath();
+
+    canvasContext.beginPath();
+    canvasContext.scale(0.6, 2.2);
+    canvasContext.arc(278, 182, 3, 0, 2 * Math.PI, false);
+    canvasContext.strokeStyle = '#37B6FA';
+    canvasContext.fillStyle = '#37B6FA';
+    canvasContext.fill();
+    canvasContext.stroke();
+    canvasContext.closePath();
+    
+    canvasContext.beginPath();
+    canvasContext.arc(361, 182, 3, 0, 2 * Math.PI, false);
+    canvasContext.strokeStyle = '#37B6FA';
+    canvasContext.fillStyle = '#37B6FA';
+    canvasContext.fill();
+    canvasContext.closePath();
+    canvasContext.stroke();
+    canvasContext.restore();
+
+    //draw mouth
+    canvasContext.beginPath();
+    canvasContext.save();
+    canvasContext.rotate(5 * Math.PI / 180);
+    canvasContext.scale(2, 0.6);
+    canvasContext.arc(110, 530, 12, 0, 2 * Math.PI);
+    canvasContext.strokeStyle = '#37B6FA';
+    canvasContext.stroke();
+    canvasContext.restore();
+    canvasContext.closePath();
+
+    //draw nose
+    canvasContext.beginPath();
+    canvasContext.moveTo(196, 278);
+    canvasContext.lineTo(187, 315);
+    canvasContext.lineTo(202, 315);
+    canvasContext.lineWidth = 2;
+    canvasContext.stroke();
+    canvasContext.closePath();    
+
+    //draw hat
+    canvasContext.beginPath();
+    canvasContext.save();
+    canvasContext.scale(2, 0.4);
+    canvasContext.arc(100, 610, 35, 0, 2 * Math.PI);
+    canvasContext.strokeStyle = 'black';
+    canvasContext.fillStyle = '#0E53B3';
+    canvasContext.fill();
+    canvasContext.stroke();
+    canvasContext.restore();
+    canvasContext.closePath();
+
+    canvasContext.beginPath();
+    canvasContext.fillStyle = '#0E53B3';
+    canvasContext.moveTo(240, 233);
+    canvasContext.lineTo(240, 150);
+    canvasContext.moveTo(240, 150);
+    canvasContext.quadraticCurveTo(205, 130, 165, 150);
+    canvasContext.quadraticCurveTo(205, 170, 240, 150);
+    canvasContext.moveTo(165, 150);
+    canvasContext.lineTo(165, 233);
+    canvasContext.quadraticCurveTo(200, 260, 240, 233);
+    canvasContext.lineTo(240, 150);
+    canvasContext.fill();
+    canvasContext.strokeStyle = 'black';
+    canvasContext.stroke();
+    canvasContext.closePath();
+
+    //draw bike
+    //no time TODO:
 }
 draw();
