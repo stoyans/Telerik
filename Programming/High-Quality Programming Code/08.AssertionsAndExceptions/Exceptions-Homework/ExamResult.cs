237310baadba72ee3cf3fee8140bbe138637f85e
Stoyan Stoyanov <fi_tanis@abv.bg>
2014-06-04 19:58:54 +0300

    update quality code hw

---
+++ Programming/High-Quality Programming Code/08.AssertionsAndExceptions/Exceptions-Homework/ExamResult.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class ExamResult
+{
+    private int grade; 
+    private int minGrade;
+    private int maxGrade;
+    private string comments;
+
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade
+    {
+        get
+        {
+            return this.grade;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("You have entered invalid value for grade!");
+            }
+
+            this.grade = value;
+        }
+    }
+
+    public int MinGrade
+    {
+        get
+        {
+            return this.minGrade;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("You have entered invalid value for mingrade!");
+            }
+
+            this.minGrade = value;
+        }
+    }
+
+    public int MaxGrade
+    {
+        get
+        {
+            return this.maxGrade;
+        }
+
+        private set
+        {
+            if (value <= this.MinGrade)
+            {
+                throw new ArgumentException("You have entered invalid value for Maxgrade!");
+            }
+
+            this.maxGrade = value;
+        }
+    }
+
+    public string Comments
+    {
+        get
+        {
+            return this.comments;
+        }
+
+        private set
+        {
+            if (value == null)
+            {
+                this.comments = string.Empty;
+            }
+
+            this.comments = value;
+        }
+    }
+}
---
+++ Programming/High-Quality Programming Code/08.AssertionsAndExceptions/Exceptions-Homework/ExceptionsHomework.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ExceptionsHomework
+{
+    public static T[] Subsequence<T>(T[] arr, int startIndex, int count)
+    {
+        if (arr.Length == 0 || arr == null)
+        {
+            throw new ArgumentException("The input array is empty. Operation is aborted!");
+        }
+        else if (startIndex > arr.Length - 1 || startIndex < 0)
+        {
+            throw new ArgumentException("The starting index is outside the array!");
+        }
+        else if (count > arr.Length)
+        {
+            throw new ArgumentException("count symbols exceeds the array length!");
+        }
+
+        List<T> result = new List<T>();
+        for (int i = startIndex; i < startIndex + count; i++)
+        {
+            result.Add(arr[i]);
+        }
+
+        return result.ToArray();
+    }
+
+    public static string ExtractEnding(string str, int count)
+    {
+        if (count > str.Length)
+        {
+            throw new ArgumentException("Length of the extracted symbols is greater than string length");
+        }
+
+        if (count < 0)
+        {
+            throw new ArgumentException("Count number must be positive");
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = str.Length - count; i < str.Length; i++)
+        {
+            result.Append(str[i]);
+        }
+
+        return result.ToString();
+    }
+
+    public static string CheckPrime(int number)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentException("The input number should be greater than 1");
+        }
+
+        for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                return string.Format("The number {0} is not prime", number);
+            }
+        }
+
+        return string.Format("The number {0} is prime", number);
+    }
+
+    public static void Main()
+    {
+        var substr = Subsequence("Hello!".ToCharArray(), 2, 3);
+        Console.WriteLine(substr);
+
+        var subarr = Subsequence(new int[] { -1, 3, 2, 1 }, 0, 2);
+        Console.WriteLine(String.Join(" ", subarr));
+
+        var allarr = Subsequence(new int[] { -1, 3, 2, 1 }, 0, 4);
+        Console.WriteLine(String.Join(" ", allarr));
+
+        var emptyarr = Subsequence(new int[] { -1, 3, 2, 1 }, 0, 0);
+        Console.WriteLine(String.Join(" ", emptyarr));
+
+        Console.WriteLine(ExtractEnding("I love C#", 2));
+        Console.WriteLine(ExtractEnding("Nakov", 4));
+        Console.WriteLine(ExtractEnding("beer", 4));
+        Console.WriteLine(ExtractEnding("Hi", 100));
+
+        //Prime Check
+        Console.WriteLine(CheckPrime(23));
+        Console.WriteLine(CheckPrime(33));
+
+        //Students Tests
+        List<Exam> peterExams = new List<Exam>()
+        {
+            new SimpleMathExam(2),
+            new CSharpExam(55),
+            new CSharpExam(100),
+            new SimpleMathExam(1),
+            new CSharpExam(0),
+        };
+
+        Student peter = new Student("Peter", "Petrov", peterExams);
+        double peterAverageResult = peter.CalcAverageExamResultInPercents();
+        Console.WriteLine("Average results = {0:p0}", peterAverageResult);
+    }
+}
