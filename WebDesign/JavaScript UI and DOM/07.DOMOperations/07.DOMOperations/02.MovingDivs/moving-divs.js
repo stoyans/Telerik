3455a46d7ebac9e93f4eb5c197beaf713093b557
Stoyan Stoyanov <fi_tanis@abv.bg>
2014-06-11 22:14:25 +0300

    DOM operations

--- WebDesign/JavaScript UI and DOM/07.DOMOperations/07.DOMOperations/02.MovingDivs/moving-divs.js
+++ WebDesign/JavaScript UI and DOM/07.DOMOperations/07.DOMOperations/02.MovingDivs/moving-divs.js
@@ -1,1 +1,39 @@
-﻿
+﻿var divs = Array(5);
+var centerX = 100,
+centerY = 150,
+radius = 100;
+var angle = 0;
+
+function createDivs() {
+    for (var i = 0; i < 5; i++) {
+
+        x = (centerX + radius * Math.cos(2 * Math.PI * i / 5)),
+        y = (centerY + radius * Math.sin(2 * Math.PI * i / 5)),
+        circleDiv = document.createElement('div');
+        circleDiv.classList.add('circle');
+        circleDiv.id = 'circle' + i;
+        circleDiv.style.left = x + 'px';
+        circleDiv.style.top = y + 'px';
+        document.body.appendChild(circleDiv);
+        divs[i] = circleDiv;
+    }
+}
+
+var functionTimer = setInterval(function moveDivs() {
+    angle++;
+    if (angle == 360) {
+        angle = 0;
+    }
+
+    for (var i = 0; i < divs.length; i++) {
+
+        var addition = (360 / divs.length) * i;
+
+        var left = centerX + Math.cos((2 * 3.14 / 180) * (angle + addition)) * radius;
+        var right = centerY + Math.sin((2 * 3.14 / 180) * (angle + addition)) * radius;
+        divs[i].style.left = left + "px";
+        divs[i].style.top = right + "px";
+    }
+}, 100);
+
+createDivs();
---
+++ WebDesign/JavaScript UI and DOM/07.DOMOperations/07.DOMOperations/04.TagsCloud/create-Tag-Cloud.js
@@ -0,0 +1,72 @@
+﻿var tags = ["cms", "javascript", "js", "ASP.NET MVC", ".net", ".net", "css", "wordpress", "xaml", "js",
+    "http", "web", "asp.net", "asp.net MVC", "ASP.NET MVC", "wp", "javascript", "js",
+    "cms", "html", "javascript", "http", "http", "CMS"];
+
+function generateTagCloud(tags) {
+    var currentTagName = '';
+    var orderedTags = [];
+    var index = 0;
+
+    for (var i = 0; i < tags.length; i++) {
+        currentTagName = tags[i].toLowerCase();
+
+        if (checkIfAdded(orderedTags, currentTagName) == true) {
+            continue;
+        }
+
+        orderedTags.push({ tagName: currentTagName, count: 0 });
+        
+        for (var j = 0; j < tags.length; j++) {
+            if (currentTagName == tags[j].toLowerCase()) {
+                orderedTags[index].count++;
+            }
+        }
+        index++;
+    }
+
+    return orderedTags;
+}
+
+function checkIfAdded(currentTags, currName) {
+    var isAvailable = false;
+    if (currentTags.length == 0) {
+        return false;
+    }
+    for (var i = 0; i < currentTags.length; i++) {
+        var curTagName = currentTags[i].tagName
+        if (currName == curTagName) {
+                return true;
+        }
+    }
+    return isAvailable;
+}
+function sortTags(tagCloud) {
+    tagCloud.sort(function (a, b) {
+        if (a.count < b.count)
+            return 1;
+        if (a.count > b.count)
+            return -1;
+        return 0;
+    });
+}
+function drawTag(tagCloud, minFontSize, maxFontSize) {
+    var fragmentDiv = document.createDocumentFragment('div');
+    for (var i = 0; i < tagCloud.length ; i++) {
+        var div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.left = random() + "px";
+        div.style.top = random() + "px";
+        div.innerHTML = tagCloud[i].tagName;
+        div.style.fontSize = (maxFontSize-3*i)+"px";
+        fragmentDiv.appendChild(div);
+    }
+    document.body.appendChild(fragmentDiv);
+}
+function random() {
+    return Math.floor((Math.random() * 200) + 0);
+}
+
+var tagCloud = generateTagCloud(tags);
+sortTags(tagCloud);
+drawTag(tagCloud, 17, 42);
+console.log(a);
